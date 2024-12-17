bc05e45a928dd62c69fe61720006d9eb3e207f02
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2024-12-16 20:29:26 -0500

    Add drawing random lines

--- src/randomline.py
+++ src/randomline.py
@@ -4,17 +4,28 @@ import pyxel
 
 width = 500
 height = 100
-randx = random.randint(1, width)
-randy = random.randint(1, height)
+borderx = 20
+bordery = 10
+y = 50
+
 pyxel.init(width, height, title="Drawing lines in Pyxel")
 
 def update():
     pass
 
-
 def draw():
+    step = 10
+    lastx = -999
+    lasty = -999
     pyxel.cls(5)
     # pyxel.rect(10, 10, 20, 20, 11)
-    pyxel.line(20, 50, randx, randy, 0)
+    # pyxel.line(20, 50, randx, randy, 0)
+    for x in range(borderx, width-borderx, step):
+        y = bordery + random.uniform(0, height - 2 * bordery)
+        if lastx > -999:
+            pyxel.line(x, y, lastx, lasty, 11)
+        lastx = x
+        lasty = y
+
 
 pyxel.run(update, draw)
