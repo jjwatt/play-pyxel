1fb4499528e82350bda5132020a4224cac4a6f77
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2024-12-21 16:09:53 -0500

    Add loadimage.py and christmas tree sprite

---
+++ src/loadimage.py
@@ -0,0 +1,24 @@
+import pyxel
+
+pyxel.init(256, 256, title="Draw Sprite")
+# load the sprite sheet
+pyxel.images[0].load(0, 0, "christmas_tree_w_snow2.png")
+
+current_frame = 0
+
+def update():
+    global current_frame
+    # Adjust speed
+    if pyxel.frame_count % 10 == 0:
+        # cycle between 0 and 1
+        current_frame = (current_frame + 1) % 2
+
+def draw():
+    pyxel.cls(0)
+    u = current_frame * 128
+    # Draw the sprite at (0, 0)
+    pyxel.blt(0, 0, 0, u, 0, 128, 160, 6)
+    pyxel.text(5, 5, f"Frame: {current_frame}", 7)
+    pyxel.text(5, 15, f"Frame Count: {pyxel.frame_count}", 7)
+
+pyxel.run(update, draw)
