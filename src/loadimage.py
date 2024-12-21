9d9306a9377cd9157b7e9d23324826feb16b982b
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2024-12-21 16:19:36 -0500

    Got a nice tree sprite centered and animated

--- src/loadimage.py
+++ src/loadimage.py
@@ -3,8 +3,9 @@ import pyxel
 pyxel.init(256, 256, title="Draw Sprite")
 # load the sprite sheet
 pyxel.images[0].load(0, 0, "christmas_tree_w_snow2.png")
-
 current_frame = 0
+tree_width = 128
+tree_height = 160
 
 def update():
     global current_frame
@@ -16,8 +17,10 @@ def update():
 def draw():
     pyxel.cls(0)
     u = current_frame * 128
-    # Draw the sprite at (0, 0)
-    pyxel.blt(0, 0, 0, u, 0, 128, 160, 6)
+    # Draw the sprite at in the x mid-point
+    tree_x = (pyxel.width // 2) - (tree_width // 2)
+    tree_y = (pyxel.height // 2) - 32
+    pyxel.blt(tree_x, tree_y, 0, u, 0, 128, 160, 6)
     pyxel.text(5, 5, f"Frame: {current_frame}", 7)
     pyxel.text(5, 15, f"Frame Count: {pyxel.frame_count}", 7)
 
