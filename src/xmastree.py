32fc7c7598032efc3009a45652c1851e29a959e9
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2024-12-21 16:20:15 -0500

    Adding other functions to xmastree

--- src/xmastree.py
+++ src/xmastree.py
@@ -2,6 +2,74 @@ import pyxel
 import random
 
 
+def bline(x1, y1, x2, y2):
+    # deltas
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # which direction?
+    x_step = 1 if dx > 0 else -1
+    y_step = 1 if dy > 0 else -1
+
+    # abs of deltas
+    dx = abs(dx)
+    dy = abs(dy)
+
+    points = []
+    # Initialize decision variable
+    if dx >= dy:
+        # line is more horizontal
+        d = 2 * dy - dx
+        while x1 != x2:
+            points.append((x1, y1))
+            if d > 0:
+                y1 += y_step
+                d -= 2 * dx
+            x1 += x_step
+            d += 2 * dy
+    else:
+        # line is more vertical
+        d = 2 * dx - dy
+        while y1 != y2:
+            points.append((x1, y1))
+            if d > 0:
+                x1 += x_step
+                d -= 2 * dy
+            y1 += y_step
+            d += 2 * dx
+    # Add the final point
+    points.append((x2, y2))
+    return points
+
+
+def draw_line(x1, y1, x2, y2, col):
+    points = bline(x1, y1, x2, y2)
+    for p in points:
+        pyxel.pset(p[0], p[1], col)
+
+
+def bcircle(x0, y0, radius):
+    x = radius
+    y = 0
+    err = 0
+    points = []
+    while x >= y:
+        points.extend([(x0 + x, y0 + y), (x0 + y, y0 + x),
+                       (x0 - y, y0 + x), (x0 - x, y0 + y),
+                       (x0 - x, y0 - y), (x0 - y, y0 - x),
+                       (x0 + y, y0 - x), (x0 + x, y0 - y)])
+        y += 1
+        err += 1 + 2 * y
+        if 2 * (err - x) + 1 > 0:
+            x -= 1
+            err += 1 - 2 * x
+    return points
+def draw_circle(x, y, radius, color):
+    points = bcircle(x, y, radius)
+    for p in points:
+        pyxel.pset(p[0], p[1], color)
+
+
 class ChristmasDemo:
     def __init__(self):
         self.width = 256
@@ -93,7 +161,8 @@ class ChristmasDemo:
         pyxel.rect(0, ground_y, self.width, 30, pyxel.COLOR_WHITE)
         speed = 15
         # Add specks of snow for texture
-        for _ in range(100):  # Adjust number of specks
+        # Adjust number of specks
+        for _ in range(100):
             if pyxel.frame_count % speed == 0:
                 x = random.randint(0, self.width)
                 y = random.randint(ground_y, self.height)
